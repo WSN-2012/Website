2089e8084bda7b582722583fd98b0f1581213c71
nataliparats <nataliparats@gmail.com>
2012-10-23 01:14:20 +0200

    Connection of web server with a database

--- src/server/Authentication.java
+++ src/server/Authentication.java
@@ -4,7 +4,8 @@ package server;
 public class Authentication {
 	public static User login(String username, String password)
 	{
-		User user = Database.login(username, password);
+		Database database = new Database("jdbc:postgresql://localhost:5432/serverdb", "postgres", "1234567890", "org.postgresql.Driver");
+		User user = database.login(username, password);
 		return user;
 
 	}
--- src/server/Database.java
+++ src/server/Database.java
@@ -1,17 +1,96 @@
 package server;
 
+import java.sql.*;
+
 public class Database {
-	
-	public static User login(String username, String password){
-		//this method checks the database and returns the User object if there if a match and null otherwise
-		
-		//just for testing:
-		if(username.equals("fabio") && password.equals("mypwd")){
-			User user = new User("fabio","mypwd","fabio@gmail.com","Fabio Viggiani");
-			return user;
+	/*
+	 * String connectionURL = "jdbc:postgresql://localhost:5432/serverdb";
+	 * String user= "postgres"; String password= "1234567890";
+	 */
+
+	Connection connection;
+	Statement statement;
+	String connectionURL;
+	String driverName;
+	String user;
+	String password;
+
+	public Database(String connectionURL, String user, String password,
+			String driverName) {
+		this.connectionURL = connectionURL;
+		this.driverName = driverName;
+		this.user = user;
+		this.password = password;
+	}
+
+	/**
+	 * Connect to the database
+	 * 
+	 * @param connectionURL
+	 *            The URL for the database connection
+	 * @param user
+	 *            The username of the database
+	 * @param password
+	 *            The password of the database
+	 * @param driverName
+	 *            The database driver class Name
+	 */
+	public void Open() {
+
+		try {
+			// Load the Driver class.
+			Class.forName(driverName);
+			// Create the connection using the static getConnection method
+			connection = DriverManager.getConnection(connectionURL, user,
+					password);
+
+			// Create a Statement class to execute the SQL statement
+			statement = connection.createStatement();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
+	}
+
+	public void Close() {
+		try {
+			statement.close();
+			connection.close();
+		} catch (SQLException ex) {
+			while (ex != null) {
+				System.out.println("SQL Exception:  " + ex.getMessage());
+				ex = ex.getNextException();
+			}
+		}
+	}
+
+	public User login(String username, String password) {
+		try {
+			// Submit a query, creating a ResultSet object
+			ResultSet rs = statement
+					.executeQuery("select username from admins where username = '"
+							+ username + "' and password = '" + password + "'");
+
+			if (rs.next()) {
+				User user = new User(rs.getString("username"),
+						rs.getString("password"), rs.getString("email"),
+						rs.getString("name"));
+				rs.close();
+				return user;
+			} else {
+				rs.close();
+				return null;
+			}
+		} catch (SQLException ex) {
+			while (ex != null) {
+				System.out.println("SQL Exception:  " + ex.getMessage());
+				ex = ex.getNextException();
+			}
+		}
 		return null;
 	}
-	
+
 }
